18ec862b95816b3a76115288bf35e82113e75a51
duzitong <duzitong811@126.com>
2016-09-11 09:58:50 +0800

    Simple GUI for Copier

---
+++ Copier.py
@@ -0,0 +1,12 @@
+class Copier():
+    def __init__(self):
+        # read rule json
+        # build dict tree
+        print("Init")
+
+    def copy(self, filePath):
+        # check dict tree
+        # force copy
+        # return copied files
+        print(filePath)
+        return []
---
+++ FlexCopier.py
@@ -0,0 +1,51 @@
+import sys
+
+from PyQt4 import QtGui, QtCore
+from Copier import *
+
+class DragDropDialog(QtGui.QDialog):
+    def __init__(self, parent):
+        super(DragDropDialog, self).__init__()
+        self.messageBrowser = QtGui.QTextBrowser(self)
+        self.messageBrowser.setGeometry(QtCore.QRect(0, 0, 290, 390))
+        self.copier = Copier()
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+        else:
+            super(DragDropDialog, self).dragEnterEvent(event)
+
+    def dragMoveEvent(self, event):
+        super(DragDropDialog, self).dragMoveEvent(event)
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            for url in event.mimeData().urls():                
+                messages = self.copier.copy(str(url.toLocalFile()))
+                for message in messages:
+                    self.messageBrowser.append(message)
+
+            event.acceptProposedAction()
+        else:
+            super(DragDropDialog,self).dropEvent(event)
+
+class MyWindow(QtGui.QWidget):
+    def __init__(self):
+        super(MyWindow,self).__init__()
+        self.setGeometry(100,100,300,400)
+        self.setWindowTitle("FlexCopier")
+
+        self.dialog = DragDropDialog(self)
+        self.dialog.setGeometry(QtCore.QRect(0, 0, 300, 400))
+        layout = QtGui.QVBoxLayout(self)
+        layout.addWidget(self.dialog)
+
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
